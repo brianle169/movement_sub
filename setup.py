92ebad459eefe59ee1c5d80c93a76f9d7b64ee52
minh_le <83758821+brianle169@users.noreply.github.com>
2024-10-23 22:56:20 -0400

    Add rclpy dependency to the package

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'movement_subscriber_exec = movement_sub.movement_subscriber_node:main',
         ],
     },
 )
