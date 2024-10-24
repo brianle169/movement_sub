c9532edfb873c88e9ad7b249498b0b0e6ca4b00f
minh_le <83758821+brianle169@users.noreply.github.com>
2024-10-23 23:46:31 -0400

    Implement message transformation from CircleMovement type to Twist type. Publish new message to turtlesim cmd_vel topic to move the turtle.

--- movement_sub/movement_subscriber_node.py
+++ movement_sub/movement_subscriber_node.py
@@ -8,9 +8,18 @@ class MovementSubscriber(Node):
         super().__init__("movement_subscriber"); # Initialize node
         self.get_logger().info("Subscriber node is activated!")
         self.subscription = self.create_subscription(CircleMovement, "movement_data", self.subscribeCallback, 10)
+        # Our subscriber is also a publisher to "turtle1/cmd_vel" topic
+        self.publisher_ = self.create_publisher(Twist, "/turtle1/cmd_vel",10)
     
     def subscribeCallback(self, msg):
-        self.get_logger().info(f"I received: linear_x {msg.linear_x}, angular_z: {msg.angular_z}")
+        self.get_logger().info(f"Received linear_x {msg.linear_x}, angular_z: {msg.angular_z}")
+        # Create a new message of type Twist to feed to /turtle1/cmd_vel topic
+        message = Twist();
+        message.linear.x = msg.linear_x
+        message.angular.z = msg.angular_z
+        self.get_logger().info(f"Publishing linear_x {msg.linear_x}, angular_z: {msg.angular_z} to /cmd_vel topic. Run \"ros2 run turtlesim turtlesim_node\" to check.")
+        self.publisher_.publish(message)
+        
 
 
 def main(args=None):
