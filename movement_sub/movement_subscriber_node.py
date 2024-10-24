e80472404a46f8af139671a7b49ca597ebe1279c
minh_le <83758821+brianle169@users.noreply.github.com>
2024-10-23 22:59:00 -0400

    Add basic class definiton and constructor

--- movement_sub/movement_subscriber_node.py
+++ movement_sub/movement_subscriber_node.py
@@ -1,6 +1,11 @@
 import rclpy
 from rclpy import Node
 
+class MovementSubscriber(Node):
+    def __init__(self):
+        super.__init__("movement_subscriber"); # Initialize node
+
+
 def main(args=None):
     # Initialize rclpy
     rclpy.init(args)
