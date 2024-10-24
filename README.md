fbbebb536c5a0fee7a0d0b24a2fe57f09cc96df6
minh_le <83758821+brianle169@users.noreply.github.com>
2024-10-23 22:42:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ROS2 package for movement data subscriber node
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'movement_sub'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='minh',
+    maintainer_email='83758821+brianle169@users.noreply.github.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
