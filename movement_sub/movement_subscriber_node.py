eac63f37ca00735fe2606f36f2f039391c9d16d2
minh_le <83758821+brianle169@users.noreply.github.com>
2024-10-23 23:27:09 -0400

    Instantiate subscription to receive the message from movement_data topic. Add Twist and CircleMovement dependencies in package.

--- movement_sub/movement_subscriber_node.py
+++ movement_sub/movement_subscriber_node.py
@@ -1,14 +1,21 @@
 import rclpy
-from rclpy import Node
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from custom_interfaces.msg import CircleMovement
 
 class MovementSubscriber(Node):
     def __init__(self):
-        super.__init__("movement_subscriber"); # Initialize node
+        super().__init__("movement_subscriber"); # Initialize node
+        self.get_logger().info("Subscriber node is activated!")
+        self.subscription = self.create_subscription(CircleMovement, "movement_data", self.subscribeCallback, 10)
+    
+    def subscribeCallback(self, msg):
+        self.get_logger().info(f"I received: linear_x {msg.linear_x}, angular_z: {msg.angular_z}")
 
 
 def main(args=None):
     # Initialize rclpy
-    rclpy.init(args)
+    rclpy.init(args=args)
     # Instantiate node and activate node
     subsrciber_node = MovementSubscriber()
     rclpy.spin(subsrciber_node)
